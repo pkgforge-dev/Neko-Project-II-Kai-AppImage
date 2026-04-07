7fb97384a2b95923e728223710b52a9eab922bfe
Link4Electronics <link4electronics@gmail.com>
2026-04-06 21:03:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,7 +16,6 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package gtk2
-#PRE_BUILD_CMDS='sed -i -e "s/patch -p1 -i \"\${srcdir}\/3d23274eefcf9c494358662539c29eebd8996cbf.patch\"/# &/" -e "s/patch -p1 -i \"\${srcdir}\/697fac371b414212526aae25e7c9774f167f4ef3.patch\"/# &/" ./PKGBUILD' make-aur-package np2kai-git
 PRE_BUILD_CMDS='sed -i -e "s/patch -p1 -i \"\${srcdir}\/3d23274eefcf9c494358662539c29eebd8996cbf.patch\"/# &/" -e "s/patch -p1 -i \"\${srcdir}\/697fac371b414212526aae25e7c9774f167f4ef3.patch\"/# &/" -e "/prepare() {/a \  sed -i \"s/MID_DBSS/MID_EN_DBSS/g\" \"\${srcdir}/NP2kai/embed/menubase/menusys.c\"" ./PKGBUILD' make-aur-package np2kai-git
 
 # If the application needs to be manually built that has to be done down here
