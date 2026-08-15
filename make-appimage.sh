928ecae0b826ba2a5221bcd2ca9339d404ae3a65
Link4Electronics <link4electronics@gmail.com>
2026-08-15 20:23:15 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q np2kai-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q np2kai-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -13,8 +13,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/xnp21kai_sdl2 /usr/bin/xnp2kai_sdl2 /usr/bin/xnp21kai_HAXM_sdl2
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
